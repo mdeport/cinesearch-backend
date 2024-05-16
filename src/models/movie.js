21111b087277328fa447b787e67be5e6cae3d70a
mdeport <mateo.deport@ecole-isitech.fr>
2024-05-16 15:24:39 +0200

    commit shema correction acteur

--- src/models/movie.js
+++ src/models/movie.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 
+const actorSchema = new Schema({
+  nom: { type: String, required: true },
+  rôle: { type: String, required: true },
+});
+
 const movieSchema = new Schema({
   titre: { type: String, required: true },
   année: { type: Number, required: true },
-  genre: { type: [String], required: true },
+  genres: { type: [String], required: true },
   réalisateur: { type: String, required: true },
-  acteurs: { type: [String], required: true },
+  acteurs: { type: [actorSchema], required: true },
 });
 
 export default model("Movie", movieSchema);
